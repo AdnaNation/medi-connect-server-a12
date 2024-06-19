d634c3bed5847f541a92cefdf1034510a78dbe57
AdnaNation <azizulislamadnan@gmail.com>
2024-06-19 19:20:20 +0600

    created payment intent

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -211,6 +212,24 @@ async function run() {
           const result = await cartCollection.deleteMany(query);
           res.send(result);
         })
+
+        // payment intent
+
+        app.post('/create-payment-intent', async (req, res) => {
+          const { price } = req.body;
+          const amount = parseInt(price * 100);
+          console.log(amount, 'amount inside the intent')
+    
+          const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'usd',
+            payment_method_types: ['card']
+          });
+    
+          res.send({
+            clientSecret: paymentIntent.client_secret
+          })
+        });
 
 
     // Send a ping to confirm a successful connection
